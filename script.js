9da160f65fae160828b2adb776523c6deca25da2
jesicadaiana <ajesicadaiana@gmail.com>
2024-01-13 11:46:51 -0300

    Colores

--- script.js
+++ script.js
@@ -48,3 +48,19 @@ document.addEventListener("DOMContentLoaded", function () {
          document.getElementById("contactForm").reset();
      }
      
+
+
+     // Función para desplazarse al principio de la página
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+// Mostrar o ocultar el icono según el desplazamiento de la página
+window.onscroll = function() {
+    var scrollButton = document.getElementById('scroll-to-top');
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollButton.style.display = 'block';
+    } else {
+        scrollButton.style.display = 'none';
+    }
+};
